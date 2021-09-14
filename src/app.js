ace93ac27f3fd54dbf291d214013d5fe1a1bb47d
swarna <swarna.gopalan@docusign.com>
2021-09-14 11:24:39 -0500

    Add heroku support

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const viewPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express()
+const port = process.env.PORT || 3000
 // set up static directory to serve
 app.use(express.static(publicDirPath))
 
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`)
 })
